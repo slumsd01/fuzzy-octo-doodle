53052ea71fddb23dd7a3ca80c486805b21f382e3
spaghettimedic <dsteiner247@gmail.com>
2022-01-17 16:04:23 -0500

    ability to include a pet on a post - updated files: models/index.js, models/Post.js, seeds/post-seeds.js, controllers/api/post-routes.js

--- seeds/pet-seeds.js
+++ seeds/pet-seeds.js
@@ -2,56 +2,56 @@ const { Pet } = require('../models');
 
 const petData = [
   {
-    pet_name: 'Sparky',
+    pet_name: 'Sparky', // id: 1
     pet_age: 3,
     pet_sex: 'male',
     pet_type: 'dog',
     user_id: 1
   },
   {
-    pet_name: 'Onyx',
+    pet_name: 'Onyx', // id: 2
     pet_age: 9,
     pet_sex: 'male',
     pet_type: 'cat',
     user_id: 2
   },
   {
-    pet_name: 'Jeffrey',
+    pet_name: 'Jeffrey', // id: 3
     pet_age: 1,
     pet_sex: 'female',
     pet_type: 'lizard/snake',
     user_id: 3
   },
   {
-    pet_name: 'Polly',
+    pet_name: 'Polly', // id: 4
     pet_age: 2,
     pet_sex: 'female',
     pet_type: 'bird',
     user_id: 4
   },
   {
-    pet_name: 'Clive',
+    pet_name: 'Clive', // id: 5
     pet_age: 1,
     pet_sex: 'male',
     pet_type: 'rodent',
     user_id: 5
   },
   {
-    pet_name: 'Clyde',
+    pet_name: 'Clyde', // id: 6
     pet_age: 2,
     pet_sex: 'male',
     pet_type: 'fish',
     user_id: 6
   },
   {
-    pet_name: 'Bonnie',
+    pet_name: 'Bonnie', // id: 7
     pet_age: 1,
     pet_sex: 'female',
     pet_type: 'plant',
     user_id: 7
   },
   {
-    pet_name: 'Jake from StateFarm',
+    pet_name: 'Jake from StateFarm', // id: 8
     pet_age: 29,
     pet_sex: 'male',
     pet_type: 'insurance representative',
