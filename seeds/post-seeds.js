f2c63501b4a221669411a83050be30e305a9757b
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-01-19 21:02:05 -0500

    able to create new user from forms

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,67 +2,67 @@ const { Post } = require('../models');
 
 const postData = [
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 7,
     pet_id: 7
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 6,
     pet_id: 6
   },
   {
-    title: 'Availability for pet sitting.',
+    title: 'Availability for pet sitting',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 7,
     pet_id: 8
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 5,
     pet_id: 5
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 4,
     pet_id: 4
   },
   {
-    title: 'Availability for pet sitting.',
+    title: 'Availability for pet sitting',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 3,
     pet_id: 3
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 3,
     pet_id: 3
   },
   {
-    title: 'Availability for pet sitting.',
+    title: 'Availability for pet sitting',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 2,
     pet_id: 2
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 2,
     pet_id: 2
   },
   {
-    title: 'Looking for pet sitter.',
+    title: 'Looking for pet sitter',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 7,
     pet_id: 7
   },
   {
-    title: 'Availability for pet sitting.',
+    title: 'Availability for pet sitting',
     post_body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate ut. Tortor vitae purus faucibus ornare suspendisse.',
     user_id: 1,
     pet_id: 1
